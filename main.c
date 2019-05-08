9cccaccb47de3b352cf1c0c97421c98897f3a135
nscamardi97 <36168900+nscamardi97@users.noreply.github.com>
2019-05-07 23:27:38 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,16 +1,18 @@
 #include <msp430.h>
 #include <stdint.h>
 
-const float Kp = 0.5;                           // Proportional Term
-const float Ki = 0.07;                          // Integral Term
-const float Kd = 0.3;                           // Derivative Term
-const int OutMax = 998, OutMin = 0;             // Max/Min Duty Cycle Possible
+const float Kp = 0.5;   // Proportional Control Constant
+const float Ki = 0.07;  // Integral Control Constant
+const float Kd = 0.3;   // Derivative Control Constant
+const int MaxOut = 998, MinOut = 0; // Maximum/Minimum Duty Cycle
+
 float Up;
 int Out;
 int uart_count = 0;
 int seconds_count = 10;
 int ADC_count = 0;
-float x1 = 0, x2 = 0, x3 = 0, x4 = 0, x5 = 0, x6 = 0, x7 = 0, x8 = 0, x9 = 0, x10 = 0, y;
+float k1 = 0, k2 = 0, k3 = 0, k4 = 0, k5 = 0, k6 = 0, k7 = 0, k8 = 0, k9 = 0, k10 = 0, y;
+
 //bi constants set for FIR filter
 const float a1 = 0.25;
 const float a2 = 0.20;
@@ -23,13 +25,13 @@ const float a8 = 0.05;
 const float a9 = 0.05;
 const float a10 = 0.05;
 
-volatile float tempVolt, potVolt;
+volatile float temp_Voltage, pot_Voltage;   // Voltage out from the PTAT (temp_Voltage) and the potentiometer (pot_Voltage)
 float temp, temp1;
 float ADC_Voltage, Vp;
 float Ud, error, i_error, OutPreSat, p_error;
 float Ui = 0;
-float desiredTemp, prevTemp;                     // Desired temperature
-uint16_t test;
+float setTemp, prevTemp;    // Desired/Set Temperature and Previous Temperature
+uint16_t test;  // Test Variable
 
 void TimerA0Setup(){
     //Set P1.0, P4.7, P1.2 to the output direction.
@@ -37,68 +39,68 @@ void TimerA0Setup(){
     P4DIR |= BIT7;
     P1DIR |= BIT2;
     P1SEL |= BIT2;
-    TA0CCTL1 = OUTMOD_7; // Enable interrupt in compare mode
+    TA0CCTL1 = OUTMOD_7;        // Enable interrupt in compare mode
     TA0CCR0 = 999;              // Set period of CCR0 1000 microseconds.
     TA0CCR1 = 500;              // Set duty cycle to 50%
     TA0CTL = TACLR;             // Clear flag
-    TA0CTL = TASSEL_1 + MC_1;   // SMCLK, UP
+    TA0CTL = TASSEL_1 + MC_1;   // TimerA0 Control: SMCLK, UP Mode
 }
 
 int updatePWM(void)
 {
     // Temperature calculations
-    tempVolt = ADC12MEM0;
-    potVolt = ADC12MEM2;
+    temp_Voltage = ADC12MEM0;   // Stores PTAT voltage in MEM0 of ADC
+    pot_Voltage = ADC12MEM2;    // Stores Potentiometer voltage in MEM2 of ADC
 
-    potVolt = ((potVolt*3.3)/4096);
-    desiredTemp = ((potVolt)*30);
-    temp1 = ((tempVolt*3.3)/4096);
+    pot_Voltage = ((pot_Voltage*3.3)/4096);     // Calculation for potentiometer voltage reading
+    setTemp = ((pot_Voltage)*30);               // Conversion from PTAT reading to temperature
+    temp1 = ((temp_Voltage*3.3)/4096);
     temp1 = (temp1*100);
 
-    // samples are shifted up
-    x10 = x9;
-    x9 = x8;
-    x8 = x7;
-    x7 = x6;
-    x6 = x5;
-    x5 = x4;
-    x4 = x3;
-    x3 = x2;
-    x2 = x1;
-    x1 = temp1;
+    // Samples shifted up by 1 and temp1 is shifted in
+    k10 = k9;
+    k9 = k8;
+    k8 = k7;
+    k7 = k6;
+    k6 = k5;
+    k5 = k4;
+    k4 = k3;
+    k3 = k2;
+    k2 = k1;
+    k1 = temp1;
 
     // FIR filter input: x  output: temp
-    temp = (x1*a1) + (x2*a2) + (x3*a3) + (x4*a4) + (x5*a5) + (x6*a6) + (x7*a7) + (x8*a8) + (x9*a9) + (x10*a10);
+    temp = (k1*a1) + (k2*a2) + (k3*a3) + (k4*a4) + (k5*a5) + (k6*a6) + (k7*a7) + (k8*a8) + (k9*a9) + (k10*a10);
 
-    p_error = error;                        // Track previous Up and current Up
-    error = temp - desiredTemp;             // Compute the error
-    Up = Kp * error;                        // Compute Proportional Control
-    Ui += (Ki * (error * 0.025));           // Compute Integral Control
-    Ud = Kd * (((error - p_error)) / 0.025);// Compute Derivative Control
+    p_error = error;                        // Track previous Up and Current Up
+    error = temp - setTemp;                 // Calculation for the error
+    Up = Kp * error;                        // Calculation for Proportional Control
+    Ui += (Ki * (error * 0.025));           // Calculation for Integral Control
+    Ud = Kd * (((error - p_error)) / 0.025);    // Calculation for Derivative Control
     Out = (Up + Ui + Ud)*30;              // Output before saturation is taken into account
 
-    //simple bang bang to speed up system process between large values
-    if(desiredTemp-temp>3){
-        Out = OutMin;
-    }else if (desiredTemp-temp<-3){
-        Out = OutMax;
+    // Simple bang bang control to speed up system process between large values
+    if(setTemp-temp>3){
+        Out = MinOut;
+    } else if (setTemp-temp<-3){
+        Out = MaxOut;
     } else {
         Out = Out;
     }
-    uart_count += 1;
+    uart_count += 1;        // Increment UART counter
     switch (uart_count)
     {
-        case 20: // send volt regulator temp
+        case 20: // Send volt regulator temperature
         {
             test = temp;
-            UCA1TXBUF = test+4;
+            UCA1TXBUF = test+4; // Insert test variable into Transmit Buffer for UART
             break;
         }
-        case 40: // send potentiometer temp
+        case 40: // Send potentiometer temperature
         {
-            test = desiredTemp;
-            UCA1TXBUF = test;
-            uart_count = 0;
+            test = setTemp;     // Set test to desired/set temperature
+            UCA1TXBUF = test;   // Insert test variable into Transmit Buffer for UART
+            uart_count = 0;     // Set UART counter to zero
             break;
         }
         default: break;
@@ -108,29 +110,31 @@ int updatePWM(void)
 
 int main(void)
 {
-  WDTCTL = WDTPW+WDTHOLD;                   // Stop watchdog timer
+  WDTCTL = WDTPW+WDTHOLD;                   // Disable the Watchdog timer
 
   TimerA0Setup();                           // Setup TimerA0
 
   P6SEL = 0x1F;                             // Enable A/D channel inputs
+  P4SEL |= BIT4;                            // UART TX
+  P4SEL |= BIT5;                            // UART RX
+
+  UCA1CTL1 |= UCSWRST;                      // Clears the UART control register 1
+  UCA1CTL1 |= UCSSEL_2;                     // Sets SMCLK
+  UCA1BR0 = 104;                            // Baud rate 9600
+  UCA1BR1 = 0;                              // Baud rate 9600
+  UCA1MCTL |= UCBRS_2;                      // Set modulation pattern to high on bit 1 & 5
+  UCA1CTL1 &= ~UCSWRST;                     // Initialize USCI
+  UCA1IE |= UCRXIE;                         // Enable USCI_A1 RX interrupt
+  UCA1IFG &= ~UCRXIFG;                      // Clears interrupt flags
+
   ADC12CTL0 = ADC12ON+ADC12MSC+ADC12SHT0_2; // Turn on ADC12, set sampling time
   ADC12CTL1 = ADC12SHP+ADC12CONSEQ_1;       // Use sampling timer, single sequence
   ADC12MCTL0 = ADC12INCH_0;                 // ref+=AVcc, channel = A0
   ADC12MCTL2 = ADC12INCH_2;                 // ref+=AVcc, channel = A2
   ADC12IE = 0x10;                           // Enable ADC12IFG.4
   ADC12CTL0 |= ADC12ENC;                    // Enable conversions
-  P4SEL |= BIT4;                            // UART TX
-  P4SEL |= BIT5;                            // UART RX
-  UCA1CTL1 |= UCSWRST;                      // Clears the UART control register 1
-  UCA1CTL1 |= UCSSEL_2;                     // Sets SMCLK
-  UCA1BR0 = 104;                            // For baud rate of 9600
-  UCA1BR1 = 0;                              // For baud rate of 9600
-  UCA1MCTL |= UCBRS_2;                      // set modulation pattern to high on bit 1 & 5
-  UCA1CTL1 &= ~UCSWRST;                     // initialize USCI
-  UCA1IE |= UCRXIE;                         // enable USCI_A1 RX interrupt
-  UCA1IFG &= ~UCRXIFG;                      // clears interrupt flags
 
-
+// Infinite While loop
   while(1)
   {
       ADC12CTL0 |= ADC12SC;
@@ -139,6 +143,7 @@ int main(void)
   }
 }
 
+// Interrupt vector for ADC
 #pragma vector=ADC12_VECTOR
 __interrupt void ADC12ISR (void)
 {
